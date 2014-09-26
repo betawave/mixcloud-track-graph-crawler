8c1d0f14d03482a74f8f2dae4c54b2349b623dc5
Leonard Hübner <leo@huebner.org>
2014-09-26 17:42:43 +0200

    adding a initializer, a query-method to send sparql-query-strings to the sparqle endpoint specified in the initializer

--- 4store.rb
+++ 4store.rb
@@ -1,4 +1,19 @@
-# this is just a streak saver :D
+require 'cgi'
+require 'net/http'
+require 'json'
 
+# this wierd name flip needed to be done as ruby does not allow leading digits in class-names
 class Store4
+  @@jsonFlag = '&output=json'
+
+  def initialize(path)
+    @sparqlEndpoint = path
+  end
+
+  def query(query)
+    protoUri = @sparqlEndpoint + '?query=' + CGI.escape(query) + @@jsonFlag
+    uri = URI.parse(protoUri)
+    resp = Net::HTTP.get(uri)
+    return JSON.parse(resp)
+  end
 end
