4bc6fc61fccbfed77bf7b7394895e14f27cf96cc
Leonard Hübner <leo@huebner.org>
2014-09-26 20:01:55 +0200

    once again splitting a working class into two as it seemed semantically correct SPARQLEndpoint for generel HTTP-SPARQL related methods/variables Store4 for 4Store specific quirks
    also adding some silly comment ;)

--- 4store.rb
+++ 4store.rb
@@ -2,18 +2,29 @@ require 'cgi'
 require 'net/http'
 require 'json'
 
-# this wierd name flip needed to be done as ruby does not allow leading digits in class-names
-class Store4
-  @@jsonFlag = '&output=json'
-
+class SPARQLEndpoint
   def initialize(path)
     @sparqlEndpoint = path
   end
 
-  def query(query)
-    protoUri = @sparqlEndpoint + '?query=' + CGI.escape(query) + @@jsonFlag
+  def query(query, *flags)
+    # the following 4 lines kind of stink in my opinion, might be reduced with proper usage of the URI-class which is able to cope with params, but it didn't quite work out with me and this probably handsom[aka usefull] class on our first date, but I spoke with my (only imaginary, but hey.. better than nothin') bffl (in case you lived under a rock for the last few years: BestFriendForLive) and we figured that I should give him a second chance some day, works for now
+    protoUri = @sparqlEndpoint + '?query=' + CGI.escape(query)
+    flags.each do |flag|
+      protoUri += flag
+    end
+
     uri = URI.parse(protoUri)
-    resp = Net::HTTP.get(uri)
-    return JSON.parse(resp)
+    return Net::HTTP.get(uri)
+  end
+end
+
+# this wierd name flip needed to be done as ruby does not allow leading digits in class-names
+class Store4 < SPARQLEndpoint
+  @@jsonFlag = '&output=json'
+
+  def query(query)
+    response = super(query, @@jsonFlag)
+    return JSON.parse(response)
   end
 end
