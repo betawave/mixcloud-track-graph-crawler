c72128faaa6eec9cabfd4625718f0d9a69121e1d
Leonard Hübner <leo@huebner.org>
2014-12-12 17:16:26 +0100

    rewriting run-method so that different prefix is used (sparql/update) depending on the kind of Keyword that starts the query Issue: this will not work in case PREFIX-Statements are used
    module at the beginning was taken from stackoverflow

--- SPARQLEndpoint.rb
+++ SPARQLEndpoint.rb
@@ -1,6 +1,17 @@
 require 'cgi'
 require 'net/http'
 
+
+# Need to not fail when uri contains curly braces
+# This overrides the DEFAULT_PARSER with the UNRESERVED key, including '{' and '}'
+# DEFAULT_PARSER is used everywhere, so its better to override it once
+module URI
+  remove_const :DEFAULT_PARSER
+  unreserved = REGEXP::PATTERN::UNRESERVED
+  DEFAULT_PARSER = Parser.new(:UNRESERVED => unreserved + "\{\}")
+end
+
+
 class SPARQLEndpoint
   # taken from: w3:/TR/sparql11-update/
   @@updateVerbs = ['INSERT', 'DELETE', 'LOAD', 'CLEAR', 'CREATE', 'DROP', 'COPY', 'MOVE', 'ADD']
@@ -13,9 +24,20 @@ class SPARQLEndpoint
   end
 
   def run(sparql, *flags)
-    sparql.split(' ')
-
-    protoUri += CGI.escape(sparql)
+    queryElements = sparql.split(' ')
+    
+    # selects corret stem depnding on first keyword in the query
+    if(@@updateVerbs.select { |verb|
+      verb == queryElements[0]
+    }.length() > 0)
+      protoUri = updateStem
+    elsif(@@queryVerbs.select { |verb|
+      verb == queryElements[0]
+    }.length() > 0)
+      protoUri = queryStem
+    end
+    
+    protoUri += queryElements.join('+')
 
     # appending all, other flags; the following 3 lines (and some others which are scattered across this class) kind of stink in my opinion, might be reduced with proper usage of the URI-class which is able to cope with params, but it didn't quite work out with me and this probably handsom[aka usefull] class on our first date, but I spoke with my bffl (although she's only imaginary, but hey.. better than nothin'; in case you lived under a rock for the last few years: BestFriendForLive) and we figured that I should give him a second chance some day, but for now I'll stick with good old string-manip even though he stinks a little when he comes back home from work, he at least dose the job without big chananigans
     flags.each do |flag|
